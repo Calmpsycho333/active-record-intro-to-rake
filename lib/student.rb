7a6755a77c0b54ad83e1b81714e0c5e717240951
calmpsycho333 <wrathking75@yahoo.com>
2021-01-22 09:39:02 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -24,6 +24,13 @@ class Student
         )
     SQL
     DB[:conn].execute(sql) 
+
+    namespace :db do
+      desc 'migrate changes to your database'
+      task :migrate => :environment do
+        Student.create_table
+      end
+    end
   end
 
   def self.drop_table
@@ -50,5 +57,5 @@ class Student
     sql = "SELECT * FROM students" 
     DB[:conn].execute(sql)
   end
-
+ 
 end
